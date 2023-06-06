8e0ad8463df408ad7127c2ba6ab8534c316ba07a
alanpgs <alan@greensmedia.com>
2023-06-06 16:51:29 +0400

    test works

--- src/ListItems.jsx
+++ src/ListItems.jsx
@@ -9,7 +9,9 @@ const ListItems = ({data}) => {
 
   return (
     <div className="table_body_item">
-       
+        <div className="h_item">{data?.display_name}</div>
+        <div className="h_item">{data?.bid.toFixed(4)}</div>
+        <div className={`h_item ${data?.average < 0 ? 'minus_' : 'plus_'}`}>{data?.average}</div>
     </div>
   )
 }
